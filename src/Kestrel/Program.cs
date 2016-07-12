727cbc30375e685caf097930a2bed4ff7266b3e1
richardkdrew <richardkdrew@users.noreply.github.com>
2016-07-12 20:44:19 +1200

    Added SSL Example code

--- src/Kestrel/Program.cs
+++ src/Kestrel/Program.cs
@@ -7,7 +7,10 @@ namespace Kestrel
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseKestrel()
+                .UseKestrel(options => {
+                    options.UseHttps("testCert.pfx", "testPassword");
+                })
+                .UseUrls("https://localhost:5000")               
                 .UseStartup<Startup>()               
                 .Build();
 
