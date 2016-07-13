1fffa2305465a5b7ab6ec0205b075ddf6bddbd02
Richard Drew <rdrew@fraedom.com>
2016-07-13 16:20:20 +1200

    Using environment variables for config

--- src/Kestrel/Program.cs
+++ src/Kestrel/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Kestrel
@@ -6,9 +7,16 @@ namespace Kestrel
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();     
+
+            var certificate = config["SSL.CERT"];
+            var password = config["SSL.CERT.PASSWORD"];
+
             var host = new WebHostBuilder()
                 .UseKestrel(options => {
-                    options.UseHttps("testCert.pfx", "testPassword");
+                    options.UseHttps(certificate, password);
                 })           
                 .UseStartup<Startup>()               
                 .Build();
