742d97c6552566b14a61d8d28020c485deb6dba8
richardkdrew <richardkdrew@users.noreply.github.com>
2016-07-13 21:06:23 +1200

    Added simple logging message

--- src/Kestrel/Program.cs
+++ src/Kestrel/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Kestrel
@@ -7,6 +8,10 @@ namespace Kestrel
     {
         public static void Main(string[] args)
         {
+            var loggerFactory = new LoggerFactory()
+                .AddConsole();
+            var logger = loggerFactory.CreateLogger<Program>();            
+
             var config = new ConfigurationBuilder()
                 .AddEnvironmentVariables(prefix: "ASPNETCORE_")
                 .Build();     
@@ -14,14 +19,19 @@ namespace Kestrel
             var certificate = config["SSL.CERT"];
             var password = config["SSL.CERT.PASSWORD"];
 
+            if(string.IsNullOrEmpty(certificate) || string.IsNullOrEmpty(password))
+            {
+                logger.LogError("No certificate and/or password supplied");
+            }
+
             var host = new WebHostBuilder()
                 .UseKestrel(options => {
                     options.UseHttps(certificate, password);
-                })           
+                })         
                 .UseStartup<Startup>()               
                 .Build();
 
-            host.Run();
+            host.Run();        
         }
     }
 }
